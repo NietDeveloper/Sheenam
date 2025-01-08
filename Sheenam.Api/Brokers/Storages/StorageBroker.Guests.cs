018b18d5bd61299f74ec28a44600ddc10e6a0d5a
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2025-01-08 16:11:31 +0500

    DATA: Model & Migrate Guests

---
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet <Guset> Gusets { get; set; }
+    }
+}
