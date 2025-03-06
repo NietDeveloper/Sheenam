c7bee8cbe4200474c12a8be7b1be28eeee9d837e
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2025-03-07 03:38:41 +0500

    ShouldThrowValidationExceptionOnAddIfGuestNullAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.validation.Add.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.validation.Add.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Models.Foundations.Guests.Exceptions;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
 {
@@ -28,6 +30,17 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
             //then
             await Assert.ThrowsAsync<GuestValidationExeption>(() =>
             addGuestTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker => 
+            broker.LogError(It.Is(SameExceptionAs(expectedGuestValidationException))),
+                Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertGuestAsync(It.IsAny<Guest>()),
+                Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
