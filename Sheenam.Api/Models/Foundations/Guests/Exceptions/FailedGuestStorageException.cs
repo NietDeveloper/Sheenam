3b5cd4615f86e4f389115889aac2c7034a599c18
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2025-03-12 03:55:32 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Sheenam.Api/Models/Foundations/Guests/Exceptions/FailedGuestStorageException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Guests.Exceptions
+{
+    public class FailedGuestStorageException : Xeption
+    {
+        public FailedGuestStorageException(Exception innerException) 
+            : base(message : "Failed guest storage error occurred, contact support",
+                  innerException)
+        { }
+    }
+}
