4335a0bc803db44253944792a6b7f691c168e71e
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2025-03-12 06:10:46 +0500

    ShouldThrowDependencyValidationOnAddIfDuplicateErrorOccursAndLogAsync -> FAIL

---
+++ Sheenam.Api/Models/Foundations/Guests/Exceptions/AlreadyExistGuestException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Guests.Exceptions
+{
+    public class AlreadyExistGuestException : Xeption
+    {
+        public AlreadyExistGuestException(Exception innerException) 
+            : base (message:"Guest already exist", innerException)
+        {}
+    }
+}
