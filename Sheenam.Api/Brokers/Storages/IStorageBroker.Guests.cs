bfb5fea762f3ccb960837dc5ab657ec9ca1c23db
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2025-03-14 04:36:48 +0500

    CODE RUB: Guset Changet To Guest

--- Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
@@ -1,9 +1,10 @@
 ﻿using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Guests;
 
 namespace Sheenam.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
-        ValueTask<Guset> InsertGuestAsync(Guset guset);
+        ValueTask<Guest> InsertGuestAsync(Guest guset);
     }
 }
--- Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -1,19 +1,20 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Sheenam.Api.Models.Foundations.Guests;
 
 namespace Sheenam.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
-        public DbSet<Guset> Gusets { get; set; }
+        public DbSet<Guest> Guests { get; set; }
 
-        public async ValueTask<Guset> InsertGuestAsync(Guset guset)
+        public async ValueTask<Guest> InsertGuestAsync(Guest guset)
         {
             using var broker = new StorageBroker(this.configuration);
 
-            EntityEntry<Guset> guestEntityEntry =
-                await broker.Gusets.AddAsync(guset);
+            EntityEntry<Guest> guestEntityEntry =
+                await broker.Guests.AddAsync(guset);
 
             await broker.SaveChangesAsync();
 
